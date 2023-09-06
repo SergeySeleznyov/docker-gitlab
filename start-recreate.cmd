6da694e5573352d92f3cdb0137dfbb27858664b6
sergey <sergey@home.my>
2023-09-06 19:24:44 +0300

    Update GitLab version to 16.1.2

--- start-recreate.cmd
+++ start-recreate.cmd
@@ -1,2 +1,6 @@
+docker-compose stop && docker-compose rm -f
+docker container rm gitlab-runner gitlab-server
+docker image rm gitlab-runner gitlab-server
+
 docker-compose up -d --force-recreate && docker-compose ps
 pause
